4b6e492adc5f80f5e8452eb82fbe9c5afd341ec2
Igor Mitrofanov <i.v.mitrofanov@ya.ru>
2024-01-16 17:22:13 +0300

    daemon

--- daemon.py
+++ daemon.py
@@ -80,20 +80,10 @@ def run_uploader():
         pivot_table.to_csv('data/main_pivot.csv', encoding='utf-8')
         all_data.to_csv('data/all_data.csv', index=False, encoding='utf-8')
 
-        upload_date = datetime.datetime.now()
-        upload_date_str = upload_date.strftime("%Y-%m-%d")
-        # Convert the pivot_table DataFrame to a list of dictionaries
-        pivot_table_data = [{'upload_date': upload_date_str, 'data' : pivot_table.to_dict(orient='records')}]
-
-        for record in pivot_table_data[0]['data']:
-            for key in list(record.keys()):
-                if not isinstance(key, str):
-                    # Convert the numeric key to a string
-                    record[str(key)] = record.pop(key)
 
         # Save the data to MongoDB using the MongoDBManager class
         mongo_manager = MongoDBManager(db_host=DB_HOST, db_port=DB_PORT, db_name=DB_NAME)
-        mongo_manager.save_data(data=pivot_table_data, table_name=DB_MAIN_TABLE_NAME)
+        mongo_manager.save_data(data=pivot_table, table_name=DB_MAIN_TABLE_NAME)
 
 
         logger.info('The data was saved in csv.')
